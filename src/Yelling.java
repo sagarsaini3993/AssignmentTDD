7d81c3278c4f0a3e3cf50dd082f8e22eda53e469
sagarsaini3993 <39058650+sagarsaini3993@users.noreply.github.com>
2019-05-23 19:22:50 -0400

    R4-Green

--- src/Yelling.java
+++ src/Yelling.java
@@ -8,8 +8,11 @@ public class Yelling {
 		}
 		
 		else if (name[0] == name[0].toUpperCase()) {
-			return name[0]+" IS YELLING";
-			
+			return name[0]+" IS YELLING";			
+		}
+		
+		else if (name.length == 2) {
+			return name[0]+ " and " +name[1]+ " are yelling";
 		}
 		return name[0]+" is yelling";
 	}
